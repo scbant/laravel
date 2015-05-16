755185228da6d023b6793047638dbf9074f270b1
scbant <scscbantang@gmail.com>
2015-05-16 20:51:14 +0800

    blog

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,11 +11,24 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+//Route::get('/', 'WelcomeController@index');
+//Route::get('home', 'HomeController@index');
+Route::get('/', 'HomeController@index');
 
-Route::get('home', 'HomeController@index');
+//Route::controllers([
+//	'auth' => 'Auth\AuthController',
+//	'password' => 'Auth\PasswordController',
+//]);
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin','middleware'=>'auth'], function()
+{
+    Route::get('/', 'AdminHomeController@index');
+    Route::resource('pages', 'PagesController');
+    Route::resource('comments', 'CommentsController');
+});
+
+Route::get('pages/{id}', 'PagesController@show');
+Route::post('comment/store', 'CommentsController@store');
